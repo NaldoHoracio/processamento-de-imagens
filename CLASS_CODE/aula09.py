cb933c81af91ce66fdda826858837087a9043f00
horacio <evonaldohs@ic.ufal.br>
2019-03-19 11:49:25 -0300

    Update aula09.

--- CLASS_CODE/aula09.py
+++ CLASS_CODE/aula09.py
@@ -5,27 +5,27 @@ Created on Thu Feb 14 09:49:55 2019
 @author: horacio
 """
 
-#
-import numpy as np
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Load a color image and visualize each channel separately
-img = cv2.imread('/home/horacio/Imagens/baboon.png', cv2.IMREAD_COLOR)
+img = cv2.imread('/home/horacio/Imagens/PDI/baboon.png', cv2.IMREAD_COLOR)
 bgr = cv2.split(img)
 
-plt.subplot('221'); plt.title('B');
-plt.imshow(bgr[0], 'gray')
-plt.subplot('222'); plt.title('G');
-plt.imshow(bgr[1],'gray')
-plt.subplot('223'); plt.title('R')
-plt.imshow(bgr[2],'gray')
-plt.subplot('224'); plt.title('Original');
-plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.subplot('221'), plt.title('B'), plt.imshow(bgr[0], 'gray')
+plt.subplot('222'), plt.title('G'), plt.imshow(bgr[1],'gray')
+plt.subplot('223'), plt.title('R'), plt.imshow(bgr[2],'gray')
+plt.subplot('224'), plt.title('Original'), plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
 plt.show()
 
+# %%
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 # Load a color image and visualize each channel separately
-img = cv2.imread('/home/horacio/Imagens/baboon.png', cv2.IMREAD_COLOR)
+img = cv2.imread('/home/horacio/Imagens/PDI/baboon.png', cv2.IMREAD_COLOR)
 #bgr2rgb = cv2.split(img)
 # img = bgr2rgb(img)
 r = [1,0,0] # red
@@ -33,7 +33,7 @@ g = [0,1,0] # green
 b = [0,0,1] # blue
 c = [0,1,1] # cyan
 m = [1,0,1] # magenta
-y = [1,1,0] #yellow
+y = [1,1,0] # yellow
 
 plt.subplot('241'); plt.title('RGB'); plt.imshow(img)
 plt.subplot('242'); plt.title('R'); plt.imshow(r * img)
@@ -43,21 +43,26 @@ plt.subplot('245'); plt.title('C'); plt.imshow(c * img)
 plt.subplot('246'); plt.title('M'); plt.imshow(m * img)
 plt.subplot('247'); plt.title('Y'); plt.imshow(y * img)
 
+#%%
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 #  Load a color imagem an visualize each channel separately
-img = cv2.imread('/home/horacio/Imagens/baboon.png', cv2.IMREAD_COLOR)
+img = cv2.imread('/home/horacio/Imagens/PDI/baboon.png', cv2.IMREAD_COLOR)
 bgr = cv2.split(img)
 colormap = cv2.COLORMAP_JET
 
-plt.subplot('221'); plt.title('B'); plt.imshow(cv2.applyColorMap[0], colormap)
-plt.subplot('222'); plt.title('G'); plt.imshow(cv2.applyColorMap[1], colormap)
-plt.subplot('223'); plt.title('R'); plt.imshow(cv2.applyColorMap[2], colormap)
-plt.subplot('224'); plt.title('Original'); plt.imshow(cv2.cvtColorMap,(img, cv2.COLOR_BGR2RGB))
+plt.subplot('221'), plt.title('B'), plt.imshow(cv2.applyColorMap[0], colormap)
+plt.subplot('222'), plt.title('G'), plt.imshow(cv2.applyColorMap[1], colormap)
+plt.subplot('223'), plt.title('R'), plt.imshow(cv2.applyColorMap[2], colormap)
+plt.subplot('224'), plt.title('Original'), plt.imshow(cv2.cvtColorMap,(img, cv2.COLOR_BGR2RGB))
 plt.show()
 
 #%%
 
-import numpy as np
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Load a color image and visualize each channel separetely
@@ -66,26 +71,34 @@ bgr = cv2.split(img)
 # colormap = cv2.COLORMAT_JET
 colormap = 1
 
-plt.subplot('221'); plt.title('B'); plt.imshow(cv2.applyColorMap(bgr[0]), colormap)
-plt.subplot('222'); plt.title('G'); plt.imshow(cv2.applyColorMap(bgr[1]), colormap)
-plt.subplot('223'); plt.title('R'); plt.imshow(cv2.applyColorMap(bgr[2]), colormap)
-plt.subplot('224'); plt.title('Original'); plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.subplot('221'), plt.title('B'), plt.imshow(cv2.applyColorMap(bgr[0]), colormap)
+plt.subplot('222'), plt.title('G'), plt.imshow(cv2.applyColorMap(bgr[1]), colormap)
+plt.subplot('223'), plt.title('R'), plt.imshow(cv2.applyColorMap(bgr[2]), colormap)
+plt.subplot('224'), plt.title('Original'), plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
 plt.show()
 
 #%%
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 # Load a color image and visualize each channel separetely
 img = cv2.imread('/home/horacio/Imagens/baboon.png', cv2.IMREAD_COLOR)
-img = 255 - img
-bgr = cv2.split(img)
+img2 = 255 - img
+bgr = cv2.split(img2)
 colormap = cv2.COLORMAP_JET
 
-plt.subplot('221'); plt.title('B'); plt.imshow(cv2.applyColorMap(bgr[0]), colormap)
-plt.subplot('222'); plt.title('G'); plt.imshow(cv2.applyColorMap(bgr[1]), colormap)
-plt.subplot('223'); plt.title('R'); plt.imshow(cv2.applyColorMap(bgr[2]), colormap)
-plt.subplot('224'); plt.title('Original'); plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.subplot('221'), plt.title('B'), plt.imshow(cv2.applyColorMap(bgr[0]), colormap)
+plt.subplot('222'), plt.title('G'), plt.imshow(cv2.applyColorMap(bgr[1]), colormap)
+plt.subplot('223'), plt.title('R'), plt.imshow(cv2.applyColorMap(bgr[2]), colormap)
+plt.subplot('224'), plt.title('Original'), plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
 plt.show()
 
 #%% Creating BGR color disks
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 rows = 1e3
 radius = rows/4
 bx = rows/2
