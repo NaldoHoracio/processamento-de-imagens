7aa7723307c8466411f9ca8b30de55e5aacac62c
Leony Oliveira <leony.oliveira.bernardo@hotmail.com>
2019-04-03 10:38:31 -0300

    Create keratodetect.py

---
+++ KeratoDetect/keratodetect.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from cnn import KeratoNet
+
+# Carregar o banco de imagens
+# Separar em dois grupos: Treino e Teste
+# Instanciar a KeratoNet
