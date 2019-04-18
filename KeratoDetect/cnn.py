709b71e3cbc33faa4ebe7699ff5a246a3752b214
horacio <evonaldohs@ic.ufal.br>
2019-04-18 11:23:40 -0300

    Update code.

--- KeratoDetect/cnn.py
+++ KeratoDetect/cnn.py
@@ -5,15 +5,57 @@ from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras.layers.core import Activation
 from keras.layers.core import Dropout
-from keras.layers.convolution import Conv2D
-from keras.layers.convolution import MaxPooling2D
+#from keras.layers.convolution import Conv2D
+from keras.layers.convolutional import Conv2D
+#from keras.layers.convolution import MaxPooling2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.layers.normalization import BatchNormalization
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_mldata
+from keras.optimizers import SGD
+
+import matplotlib.pyplot as plt
+import numpy as np
+import glob as gl
+import cv2 as cv
+import os
+
 RGB = 3
 GRAY = 1
 
-class KeratoNet:
+path_db = "/home/horacio/Documentos/4-PROCESSAMENTO_DE_IMAGENS-8-/KeratoDetect/DataBase/Eyes"
 
+class KeratoNet:
+    # HEIGHT = ALTURA DA IMG; WIDTH = COMPRIMENTO DA IMG; CHANNELS = RGB;
+    # CLASSES = NUM DE CLASSIFICAÇÃO - NORMAL OU KT
     def buildNet(height, width, channels, classes):
+        # Carregando imagens
+        fold_eye = gl.glob(path_db)
+
+        imgs_eyes_list = []
+
+        for fold in fold_eye:
+            for f in gl.glob(fold + '/*.png'):
+                imgs_eyes_list.append(f)
+
+        read_fold = []
+        for img in imgs_eyes_list:
+            read_fold.append(cv.imread(img, cv.IMREAD_COLOR))
+#        
+#        # normalizar todos pixels, de forma que os valores estejam
+#        # no intervalor [0, 1.0]
+#        data = []
+#        #data = read_fold("int")/255
+#                     
+#            
+#        # dividir o dataset entre train (75%) e test (25%)
+#        (trainX, testX, trainY, testY) = train_test_split(data, img_eyes_list.target)
+# 
+#        # converter labels de inteiros para vetores
+#        lb = LabelBinarizer()
+#        trainY = lb.fit_transform(trainY)
+#        testY = lb.transform(testY)
+
         # Declaramos a nossa rede seguindo um modelo sequencial.
         model = Sequential()
 
@@ -46,5 +88,27 @@ class KeratoNet:
         model.add(Dropout(0.5))
         model.add(Dense(classes))
         model.add(Activation('softmax'))
+        
+#        print("[INFO] treinando a rede neural...")
+#        model.compile(optimizer=SGD(0.01), loss="categorical_crossentropy", metrics=["accuracy"])
+#        H = model.fit(trainX, trainY, batch_size=128, epochs=10, verbose=2, validation_data=(testX, testY))
+#        
+#        # avaliar a Rede Neural
+#        print("[INFO] avaliando a rede neural...")
+#        predictions = model.predict(testX, batch_size=128)
+#        print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1)))
+#        
+#        # plotar loss e accuracy para os datasets 'train' e 'test'
+#        plt.style.use("ggplot")
+#        plt.figure()
+#        plt.plot(np.arange(0,100), H.history["loss"], label="train_loss")
+#        plt.plot(np.arange(0,100), H.history["val_loss"], label="val_loss")
+#        plt.plot(np.arange(0,100), H.history["acc"], label="train_acc")
+#        plt.plot(np.arange(0,100), H.history["val_acc"], label="val_acc")
+#        plt.title("Training Loss and Accuracy")
+#        plt.xlabel("Epoch #")
+#        plt.ylabel("Loss/Accuracy")
+#        plt.legend()
+#        plt.show()
 
         return model
--- KeratoDetect/keratodetect.py
+++ KeratoDetect/keratodetect.py
@@ -1,8 +1,103 @@
-import cv2 as cv
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.datasets import fetch_mldata
+from keras.layers.core import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
+
 import matplotlib.pyplot as plt
 from cnn import KeratoNet
+import numpy as np
+import glob as gl
+import cv2 as cv
+import os
 
-# Carregar o banco de imagens
-# Separar em dois grupos: Treino e Teste
+# Carregar o banco de imagens ^ separar em dois grupos
+# Ok!
+
+#path_ne = "/home/horacio/Documentos/4-PROCESSAMENTO_DE_IMAGENS-8-/KeratoDetect/DataBase/Normal_Eyes"
+#path_kt = "/home/horacio/Documentos/4-PROCESSAMENTO_DE_IMAGENS-8-/KeratoDetect/DataBase/Kt_Eyes"
+
+path_db = "/home/horacio/Documentos/4-PROCESSAMENTO_DE_IMAGENS-8-/KeratoDetect/DataBase/Eyes"
+## Separar em dois grupos: Treino e Teste
+#
+## Carregando imagens
+#fold_eye = gl.glob(path_db)
+#
+#imgs_eyes_list = []
+#
+#for fold in fold_eye:
+#    for f in gl.glob(fold + '/*.png'):
+#        imgs_eyes_list.append(f)
+#
+#read_fold = []
+#
+#for img in imgs_eyes_list:
+#    read_fold.append(cv.imread(img, cv.IMREAD_COLOR))
+
+#print(read_fold)
+
+kn_cnn = KeratoNet.buildNet(180,240,3,2)
+
+## plotar loss e accuracy para os datasets 'train' e 'test'
+#print("[INFO] treinando a rede neural...")
+#model.compile(optimizer=SGD(0.01), loss="categorical_crossentropy",
+#             metrics=["accuracy"])
+#H = model.fit(trainX, trainY, batch_size=128, epochs=10, verbose=2,
+#         validation_data=(testX, testY))
+#
+## avaliar a Rede Neural
+#print("[INFO] avaliando a rede neural...")
+#predictions = model.predict(testX, batch_size=128)
+#print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1)))
+#
+## plotar loss e accuracy para os datasets 'train' e 'test'
+#plt.style.use("ggplot")
+#plt.figure()
+#plt.plot(np.arange(0,100), H.history["loss"], label="train_loss")
+#plt.plot(np.arange(0,100), H.history["val_loss"], label="val_loss")
+#plt.plot(np.arange(0,100), H.history["acc"], label="train_acc")
+#plt.plot(np.arange(0,100), H.history["val_acc"], label="val_acc")
+#plt.title("Training Loss and Accuracy")
+#plt.xlabel("Epoch #")
+#plt.ylabel("Loss/Accuracy")
+#plt.legend()
+#plt.show()
+
+
+## Carregando imagens de olhos saudáveis
+#fold_normal_eye = gl.glob(path_ne)
+#
+#imgs_normal_eyes_list = []
+#
+#for fold_ne in fold_normal_eye:
+#    for f_ne in gl.glob(fold_ne + '/*.png'):
+#        imgs_normal_eyes_list.append(f_ne)
+#
+#read_ne = []
+#
+#for img_ne in imgs_normal_eyes_list:
+#    read_ne.append(cv.imread(img_ne, cv.IMREAD_COLOR))
+#
+##print(read_ne)
+##print("\n")
+#
+## Carregando imagens de olhos com kt
+#fold_kt_eye = gl.glob(path_kt)
+#
+#imgs_kt_eyes_list = []
+#
+#for fold_kt in fold_kt_eye:
+#    for f_kt in gl.glob(fold_kt + '/*.jpg'):
+#        imgs_kt_eyes_list.append(f_kt)
+#
+#read_kt = []
+#
+#for img_kt in imgs_kt_eyes_list:
+#    read_kt.append(cv.imread(img_kt, cv.IMREAD_COLOR))
+
+#print(read_kt)
+
 # Instanciar a KeratoNet
+#kn_cnn = KeratoNet.buildNet(180,240,3,2)
